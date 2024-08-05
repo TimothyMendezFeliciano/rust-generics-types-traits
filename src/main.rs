bce18ffd88ed4d7be6103565145d72e165d5df9c
TimothyMendezFeliciano <timothy.mendez@upr.edu>
2024-08-05 19:05:07 -0400

    In Functions Generic Definition

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,23 @@
 fn main() {
-    println!("Hello, world!");
+    let number_list = vec![34, 50, 25, 100, 65];
+    let result = largest(&number_list);
+
+    println!("The largest number is {result}");
+
+    let number_list = vec![102, 34, 6000, 89, 54, 2, 43, 8];
+    let result = largest(&number_list);
+
+    println!("The largest number is {result}");
+}
+
+fn largest<T>(list: &[T]) -> &T {
+    let mut largest = &list[0];
+
+    for number in &list {
+        if number > largest {
+            largest = number;
+        }
+    }
+
+    return largest;
 }
