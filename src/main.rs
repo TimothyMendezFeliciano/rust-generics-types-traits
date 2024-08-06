ed309c4163f1cf7fb76ef640ac6bac4fdb2bf2d5
TimothyMendezFeliciano <timothy.mendez@upr.edu>
2024-08-06 15:16:52 -0400

    m

--- src/main.rs
+++ src/main.rs
@@ -3,10 +3,24 @@ struct Point<T> {
     y: T,
 }
 
-struct BothPoint<T,U> {
-    x:T,
-    y:U,
+struct BothPoint<T, U> {
+    x: T,
+    y: U,
 }
+
+impl<T> Point<T> {
+    fn x(&self) -> &T {
+        &self.x
+    }
+}
+
+impl<f32> Point<f32> {
+    fn distance_from_original(&self) -> f32 {
+        (self.x.powi(2) + self.y.powi(2)).sqrt()
+    }
+}
+
+
 
 fn main() {
     let number_list = vec![34, 50, 25, 100, 65];
@@ -19,10 +33,12 @@ fn main() {
 
     println!("The largest number is {result}");
 
-    let integer = Point {x:5, y: 10}; // Must be both of the same time.
-    let float = Point {x:1.0, y: 5.0}; // Can be of different type. Only x, and y need to match type.
+    let integer = Point { x: 5, y: 10 }; // Must be both of the same time.
+    let float = Point { x: 1.0, y: 5.0 }; // Can be of different type. Only x, and y need to match type.
 
-    let integer_and_float = BothPoint {x: 1, y: 4.0};
+    let integer_and_float = BothPoint { x: 1, y: 4.0 };
+
+    println!("integer.x = {}", integer.x());
 }
 
 fn largest<T>(list: &[T]) -> &T {
